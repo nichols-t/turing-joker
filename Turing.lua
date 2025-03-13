8ce26642c12c8259f9242e91e0e9e42a30f4f9c2
nichols-t <tynichols0@gmail.com>
2025-03-12 22:44:24 -0400

    me when I make infinite loops less blocking

--- Turing.lua
+++ Turing.lua
@@ -24,6 +24,99 @@ turing_state_transitions = {
 B=--[[ B ]] { 'AR', 'KR', 'QL', 'JR', 'TL', '9R', '8L', '7R', '6L', '5R', '4L', '3R', '2L' },
 }
 
+function turing_step(state, tape_index, iterations)
+  if state == nil then
+    return nil
+  end
+  sendInfoMessage('Number of Steps: '..iterations)
+  sendInfoMessage('Current State: '..state)
+  sendInfoMessage('Tape Index: '..tape_index)
+  -- get current symbol and next state
+  local current_symbol = nil
+  local next_state = nil
+  -- note this may be nil
+  local current_card = nil
+  -- tape_index in [, #G.hand.cards] means card is there, otherwise it's blank
+  if tape_index > #G.hand.cards or tape_index < 1 then
+    current_symbol = 'B'
+    -- todo state transition from a blank symbol == increment state?
+    -- todo testing: blank sends to state end? keep getting loops
+    next_state = #G.jokers.cards;
+  else
+    current_card = G.hand.cards[tonumber(tape_index)]
+    current_symbol = current_card:get_id()
+    next_state = (current_symbol % #G.jokers.cards) + 1;
+    -- terminate on a stone card
+    if current_card.ability.effect == 'Stone Card' then
+      return true
+    end
+  end
+
+  -- aces stored at 1 not 14
+  if current_symbol == 14 then current_symbol = 1 end
+
+  sendInfoMessage('Current Symbol: '..current_symbol)
+  sendInfoMessage('Next State: '..next_state)
+
+  -- given current symbol, read the write value and the tape direction
+  local write_symbol = string.sub(turing_state_transitions[current_symbol][state], 1, 1)
+  local tape_direction_string = string.sub(turing_state_transitions[current_symbol][state], 2)
+  local tape_direction = 1
+  if tape_direction_string == 'L' then
+    tape_direction = -1
+  end
+
+  sendInfoMessage('Writing Symbol: '..write_symbol)
+  sendInfoMessage('Current State: '..state)
+  sendInfoMessage('Next State: '..next_state)
+  
+  -- store edition of current state as we'll need to write this to a card
+  local current_state_edition = G.jokers.cards[state].edition
+  -- ensure we have a "current card" on blank tape index by making a new one
+  -- rank is the symbol we said we would write, and suit is random
+  if current_card == nil then
+    local new_card_suit = nil
+    new_card_suit = pseudorandom_element({'S','H','D','C'}, pseudoseed('turing'))
+    current_card = create_playing_card(
+      {
+        front = G.P_CARDS[new_card_suit..'_'..write_symbol]
+      },
+      G.hand,
+      nil,
+      nil,
+      {G.C.SET.Default}
+    )
+  end
+
+  local next_tape_index = tape_index + tape_direction;
+  sendInfoMessage("CT: "..tape_index.." NT: "..next_tape_index)
+  -- update variables for the next iteration
+  -- state = next_state
+  -- tape_index = tape_index + tape_direction
+  -- iterations = iterations + 1
+
+  -- add the edition to the existing/new card if needed
+  G.E_MANAGER:add_event(Event({
+    func = function()
+      -- can happen on stone I think?
+      if current_card.base.suit ~= nil then
+        local suit_prefix = string.sub(current_card.base.suit, 1, 1)..'_'
+        sendInfoMessage('Setting card to '..suit_prefix..write_symbol)
+        current_card:set_base(G.P_CARDS[suit_prefix..write_symbol])
+      end
+      current_card:juice_up()
+      if write_symbol == 'S' then
+        current_card:set_ability(G.P_CENTERS['m_stone']);
+      elseif current_state_edition ~= nil then
+        current_card:set_edition(current_state_edition, true, true)
+      end
+      card_eval_status_text(current_card, 'extra', nil, nil, nil, {message = "Programmed!"})
+      turing_step(next_state, next_tape_index, iterations + 1)
+      return true
+    end
+  }))
+end
+
 
 SMODS.Joker {
   key = "turingjoker",
@@ -44,92 +137,17 @@ SMODS.Joker {
   -- Scoring calculation
   calculate = function(self, card, context)
     if context.before then
-      local state = 1 -- todo read from G.jokers.cards
-      local tape_index = 1
-
-      local iterations = 1
-      
-      while state ~= nil do
-        sendInfoMessage('Number of Steps: '..iterations)
-        sendInfoMessage('Current State: '..state)
-        sendInfoMessage('Tape Index: '..tape_index)
-        -- get current symbol and next state
-        local current_symbol = nil
-        local next_state = nil
-        -- note this may be nil
-        local current_card = nil
-        -- tape_index in [, #G.hand.cards] means card is there, otherwise it's blank
-        if tape_index > #G.hand.cards or tape_index < 1 then
-          current_symbol = 'B'
-          -- todo state transition from a blank symbol == increment state?
-          next_state = (state  + 1) % #G.jokers.cards + 1;
-        else
-          current_card = G.hand.cards[tonumber(tape_index)]
-          current_symbol = current_card:get_id()
-          next_state = current_symbol % #G.jokers.cards + 1;
-          -- terminate on a stone card
-          if current_card.ability.effect == 'Stone Card' then
-            next_state = nil
-            break
-          end
+      local state = 1
+      for k,v in ipairs(G.jokers.cards) do
+        if v == card then
+          state = k
         end
-
-        -- aces stored at 1 not 14
-        if current_symbol == 14 then current_symbol = 1 end
-
-        sendInfoMessage('Current Symbol: '..current_symbol)
-        sendInfoMessage('Next State: '..next_state)
-
-        -- given current symbol, read the write value and the tape direction
-        local write_symbol = string.sub(turing_state_transitions[current_symbol][state], 1, 1)
-        local tape_direction_string = string.sub(turing_state_transitions[current_symbol][state], 2)
-        local tape_direction = 1
-        if tape_direction_string == 'L' then
-          tape_direction = -1
-        end
-
-        sendInfoMessage('Writing Symbol: '..write_symbol)
-        sendInfoMessage('Current State: '..state)
-        sendInfoMessage('Next State: '..next_state)
-        
-        -- store edition of current state as we'll need to write this to a card
-        local current_state_edition = G.jokers.cards[state].edition
-        -- ensure we have a "current card" on blank tape index by making a new one
-        -- rank is the symbol we said we would write, and suit is random
-        if current_card == nil then
-          local new_card_suit = nil
-          new_card_suit = pseudorandom_element({'S','H','D','C'}, pseudoseed('turing'))
-          current_card = create_playing_card(
-            {
-              front = G.P_CARDS[new_card_suit..'_'..write_symbol]
-            },
-            G.hand,
-            nil,
-            nil,
-            {G.C.SET.Default}
-          )
-        end
-
-        -- update variables for the next iteration
-        state = next_state
-        tape_index = tape_index + tape_direction
-        iterations = iterations + 1
-
-        -- add the edition to the existing/new card if needed
-        G.E_MANAGER:add_event(Event({
-          trigger = 'after',
-          delay = 1,
-          func = function()
-            if current_state_edition ~= nil then
-              current_card:juice_up()
-              current_card:set_edition(current_state_edition, true, true)
-            end
-            return true
-          end
-        }))
-        card_eval_status_text(current_card, 'extra', nil, nil, nil, {message = "Programmed!"})
       end
+      local tape_index = 1
+      local iterations = 1
+
       
+      turing_step(state, tape_index, iterations)
       playing_card_joker_effects({true})
 
       return {
